4ce7f716ff5235bb306a7b1d89c2ffda951fa0ac
Priyasha Sabbavarapu <priyashadevi@gmail.com>
2025-08-28 22:14:35 +0530

    Create backspace_string_compare.py

---
+++ backspace_string_compare.py
@@ -0,0 +1,26 @@
+
+#https://leetcode.com/problems/backspace-string-compare/description/
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def func(strs,stack):
+            for i in strs:
+                if i =='#':
+                    if stack!=[]:
+                        stack.pop()
+                else:
+                    stack.append(i)
+            return "".join(stack)
+
+        stack_s=[]
+        stack_t=[]
+
+        x=func(s,stack_s)
+        y=func(t,stack_t)
+        print(x,y)
+        if x==y:
+            return True
+        else:
+            return False
+
+
